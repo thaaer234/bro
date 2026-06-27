94a03bb805aa0ca8851957551d6f7db03dd20f85
thaaer234 <mhmadwerc8@gmail.com>
2026-06-27 14:08:05 +0300

    Update production fixes

--- alyaman/settings.py
+++ alyaman/settings.py
@@ -248,7 +248,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
         "OPTIONS": {
-            "timeout": 30,
+            "timeout": 300,
         },
     }
 }
