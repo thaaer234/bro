a68103c1ac5815fcad677494a6de4d2327e3fa70
thaaer234 <mhmadwerc8@gmail.com>
2026-06-27 17:07:34 +0300

    Update production fixes

--- alyaman/wsgi.py
+++ alyaman/wsgi.py
@@ -11,12 +11,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# أضف مسار المشروع إلى Python path
-path = '/home/Aghead/yaman'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yaman.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alyaman.settings')
 
 WSGIPassAuthorization = True
 application = get_wsgi_application()
